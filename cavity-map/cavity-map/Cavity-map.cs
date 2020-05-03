c6a446abe0c6038b07070b77248a6f837621774a
ShwetaC93 <64632278+ShwetaC93@users.noreply.github.com>
2020-05-03 18:38:28 +0530

    LeetCode Work

---
+++ cavity-map/cavity-map/Cavity-map.cs
@@ -0,0 +1,71 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the cavityMap function below.
+    static void cavityMap(string[] grid)
+    {
+        for (int row = 1; (row < (grid.Length - 1)); row++)
+        {
+            for (int column = 1; (column < (grid.Length - 1)); column++)
+            {
+                int cellDepth = grid[row][column]; //1 1 9
+                if (((cellDepth > grid[row][(column + 1)])    //1>1 9 >1
+                            && ((cellDepth > grid[row][(column - 1)]) //1>1  9>1
+                            && ((cellDepth > grid[(row - 1)][column]) //1>1  9>8
+                            && (cellDepth > grid[(row + 1)][column]))))) //1>1 9>1
+                {
+                    char[] ch = grid[row].ToCharArray();
+                    ch[column] = 'X';
+                    grid[row] = new string(ch);
+                }
+
+            }
+
+        }
+
+        for (int row = 0; (row < grid.Length); row++)
+        {
+           // Console.WriteLine(grid[row].ToCharArray());
+            Console.WriteLine(string.Join("\n\n", grid[row]));
+            //Console.ReadLine();
+        }
+        Console.ReadLine();
+        //return grid;
+    }
+
+    static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string[] grid = new string[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            string gridItem = Console.ReadLine();
+            grid[i] = gridItem;
+        }
+
+       cavityMap(grid);
+
+        //Console.WriteLine(string.Join("\n", result));
+        Console.ReadLine();
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
---
+++ cavity-map/cavity-map/Two Sum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//Given nums = [2, 7, 11, 15], target = 9,
+
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+namespace cavity_map
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int i = 0;
+            int j = 1;
+            for (i = 0; i < nums.Length; i++)
+            {
+                for (j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] == target - nums[i])
+                        return new int[] { i, j };
+                }
+            }
+            return new int[] { i, j };
+        }
+    }
+}
